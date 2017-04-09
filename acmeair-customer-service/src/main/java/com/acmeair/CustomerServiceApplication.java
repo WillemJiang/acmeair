024171929b32f8df57127cd0e0b254102911e373
Willem Jiang <willem.jiang@gmail.com>
2017-04-09 22:32:55 +0800

    Added the spring cloud consul as the service discovery

--- acmeair-customer-service/src/main/java/com/acmeair/CustomerServiceApplication.java
+++ acmeair-customer-service/src/main/java/com/acmeair/CustomerServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import javax.servlet.ServletContext;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CustomerServiceApplication extends SpringBootServletInitializer {
     
