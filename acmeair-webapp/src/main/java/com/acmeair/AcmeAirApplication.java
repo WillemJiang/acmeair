22f82d06dbc709bed39fc98ceed5038a341bcc96
Willem Jiang <willem.jiang@gmail.com>
2017-04-10 08:36:26 +0800

    [WIP] Service discovery, startup the customer service in docker and update UserCommand

--- acmeair-webapp/src/main/java/com/acmeair/AcmeAirApplication.java
+++ acmeair-webapp/src/main/java/com/acmeair/AcmeAirApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 import javax.servlet.ServletContext;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class AcmeAirApplication extends SpringBootServletInitializer {
     
