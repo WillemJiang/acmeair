f0f2319e3c1673cbe6734dae8a9ee952bb8a707f
seanyinx <sean.yin@huawei.com>
2017-06-01 10:33:15 +0800

    JAV-27 updated services with service comb annotation

--- acmeair-booking-service/src/main/java/com/acmeair/AcmeAirApplication.java
+++ acmeair-booking-service/src/main/java/com/acmeair/AcmeAirApplication.java
@@ -1,11 +1,10 @@
 package com.acmeair;
 
+import io.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
@@ -23,8 +22,7 @@ public class AcmeAirApplication {
 
     @Configuration
     @Profile("cse")
-    @ImportResource("classpath*:META-INF/spring/*.bean.xml")
-    @ComponentScan(basePackages = {"com.acmeair", "io.servicecomb"})
+    @EnableServiceComb
     class CseConfig {
     }
 }
