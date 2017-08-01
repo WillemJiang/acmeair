5a2764e30a3e9bb0be4056d20cf315235d29683b
eric-lee-ltk <eric.lee.ltk@gmail.com>
2017-08-01 17:26:58 +0800

    replaced website with webapp (cherry picked from commit ec8d03b)

--- acmeair-website/src/main/java/com/acmeair/hystrix/AuthenticationCommand.java
+++ acmeair-website/src/main/java/com/acmeair/hystrix/AuthenticationCommand.java
@@ -1,71 +1,73 @@
-package com.acmeair.auth;
+package com.acmeair.hystrix;
 
-import java.io.IOException;
-
+import com.acmeair.entities.CustomerSession;
+import com.acmeair.service.AuthenticationService;
+import com.acmeair.service.UserService;
+import com.acmeair.web.dto.CustomerInfo;
+import com.acmeair.web.dto.CustomerSessionInfo;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import com.acmeair.entities.CustomerSession;
-import com.acmeair.service.AcmLoadbalance;
-import com.acmeair.service.AuthenticationService;
-import com.acmeair.web.dto.CustomerSessionInfo;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 
 
+@Service
 public class AuthenticationCommand implements AuthenticationService {
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationCommand.class);
 
     private RestTemplate restTemplate = new RestTemplate();
 
+    @Autowired
+    private LoadBalancerClient loadBalancer;
 
-    private AcmLoadbalance loadBalancer;
-
-    @Value("${customer.service.name:customers}")
+    @Value("${customer.service.name:customerServiceApp}")
     private String customerServiceName;
 
-    public AuthenticationCommand() {
+    AuthenticationCommand() {
         restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
             public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
                 return false;
             }
 
+            @Override
             public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
             }
         });
     }
     
-    @Hystrix
-    public CustomerSession validateCustomerSession(String sessionId) { 
-    	try {
-            ResponseEntity<CustomerSessionInfo> responseEntity = restTemplate.postForEntity(
-                    getCustomerServiceAddress() + "/api/login/validate",
-                    validationRequest(sessionId),
-                    CustomerSessionInfo.class
-            );
-            return responseEntity.getBody();
-    	} catch (Exception e) {
-    		logger.error("Failed in token validate.");
-    		return null;
-    	}
-
-        
+    @HystrixCommand
+    public CustomerSession validateCustomerSession(String sessionId) {
+        ResponseEntity<CustomerSessionInfo> responseEntity = restTemplate.postForEntity(
+                getCustomerServiceAddress() + "/rest/api/login/validate",
+                validationRequest(sessionId),
+                CustomerSessionInfo.class
+        );
+        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
+            logger.warn("No such customer found with session id {}", sessionId);
+            return null;
+        }
+        return responseEntity.getBody();
     }
-    
+
     protected String getCustomerServiceAddress() {
-        loadBalancer =  new AcmLoadbalance("customerServiceApp");
-        String address = loadBalancer.choose().toString();
-        String[] parts = address.split(":");
-        address = "http://" + parts[0] + ":" + parts[1];
+        String address = loadBalancer.choose(customerServiceName).getUri().toString();
         logger.info("Just get the address {} from LoadBalancer.", address);
         return address;
     }
@@ -74,9 +76,9 @@ public class AuthenticationCommand implements AuthenticationService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("sessionId", sessionId);
         
-        return new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        return new HttpEntity<>(map, headers);
     }
 }
