1bb4fccbcff5862014faf5c666e0d9f9b5362b35
eric-lee-ltk <eric.lee.ltk@gmail.com>
2017-08-01 18:44:02 +0800

    added zuul rewrite url filter (cherry picked from commit 8d5d501)

---
+++ acmeair-website/src/main/java/com/acmeair/web/UriRewriteFilter.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright 2017 Huawei Technologies Co., Ltd
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.acmeair.web;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UriRewriteFilter extends ZuulFilter {
+  private static final Logger logger = LoggerFactory.getLogger(UriRewriteFilter.class);
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 6;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+    String uri = request.getRequestURI();
+    if (uri.contains("/rest/")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    String uri = ctx.getRequest().getRequestURI();
+    int position = uri.indexOf("/rest");
+    if (position == -1) {
+      return null;
+    }
+    String newUri = uri.substring(position + 5);
+    ctx.put("requestURI", newUri);
+
+    return null;
+  }
+}
