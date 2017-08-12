c044b0fba376f295bed163ecf0f62c2be081fc2b
eric-lee-ltk <lidagang1@huawei.com>
2017-08-12 17:25:43 +0800

    fixed log error

--- acmeair-booking-service/src/main/java/com/acmeair/AcmeAirApplication.java
+++ acmeair-booking-service/src/main/java/com/acmeair/AcmeAirApplication.java
@@ -1,5 +1,6 @@
 package com.acmeair;
 
+import io.servicecomb.foundation.common.utils.Log4jUtils;
 import io.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Profile;
 @SpringBootApplication
 public class AcmeAirApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
         SpringApplication.run(AcmeAirApplication.class, args);
     }
     
