59897946be671b4cb947d1d400a7e40294719bc1
TankTian <chd.tianming@gmail.com>
2017-08-15 22:27:15 +0800

    fix the error path.

--- scripts/make_docker_image/make_docker_image.sh
+++ scripts/make_docker_image/make_docker_image.sh
@@ -5,10 +5,10 @@ mvn clean  install -DskipTests  -PHuaweiCloud
 cd "${CUR_PATH}"
 cp ./customer/Dockerfile  ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
 cp ./booking/Dockerfile  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
-cp ./webapp/Dockerfile   ${ROOT_PATH}/acmeair-webapp/target/
+cp ./webapp/Dockerfile   ${ROOT_PATH}/acmeair-webapp/target/acmeair/
 cd ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
 docker build -t acmeair-customer-service:3.0.0-SNAPSHOT  . 
-cd  ${ROOT_PATH}/acmeair/acmeair-booking-service/target/acmeair/
+cd  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
 docker build -t acmeair-booking-service:3.0.0-SNAPSHOT .
-cd ${ROOT_PATH}/acmeair/acmeair-webapp/target/
+cd ${ROOT_PATH}/acmeair-webapp/target/acmeair
 docker build -t acmeair-webapp:3.0.0-SNAPSHOT  .
