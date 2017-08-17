60a65ca13fdea695a746f7bd66068892b49c7b28
TankTian <chd.tianming@gmail.com>
2017-08-17 14:29:36 +0800

    format the dockerfile.

--- scripts/make_docker_image/make_docker_image.sh
+++ scripts/make_docker_image/make_docker_image.sh
@@ -1,14 +1,14 @@
 CUR_PATH=$(cd "$(dirname "$0")"; pwd)
 ROOT_PATH="${CUR_PATH}/../../"
 cd "${ROOT_PATH}"
-mvn clean  install -DskipTests  -PHuaweiCloud
+mvn clean package -DskipTests -DskipITs -PHuaweiCloud
 cd "${CUR_PATH}"
-cp ./customer/Dockerfile  ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
-cp ./booking/Dockerfile  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
-cp ./webapp/Dockerfile   ${ROOT_PATH}/acmeair-webapp/target/acmeair/
+cp ./Dockerfile  ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
+cp ./Dockerfile  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
+cp ./Dockerfile   ${ROOT_PATH}/acmeair-webapp/target/acmeair/
 cd ${ROOT_PATH}/acmeair-customer-service/target/acmeair/
-docker build -t acmeair-customer-service:3.0.0-SNAPSHOT  . 
+docker build -t acmeair-customer-service:0.0.1  . 
 cd  ${ROOT_PATH}/acmeair-booking-service/target/acmeair/
-docker build -t acmeair-booking-service:3.0.0-SNAPSHOT .
+docker build -t acmeair-booking-service:0.0.1 .
 cd ${ROOT_PATH}/acmeair-webapp/target/acmeair
-docker build -t acmeair-webapp:3.0.0-SNAPSHOT  .
+docker build -t acmeair-webapp:0.0.1  .
