f16427506f515bbda1510b0000790a3ed47d38ac
zhengyangyong <yangyong.zheng@huawei.com>
2017-09-27 15:39:36 +0800

    JAV-273 add SeckillCommand SpringCloud

---
+++ acmeair-customer-service/src/main/java/com/acmeair/hystrix/SpringCloudSeckillCommand.java
@@ -0,0 +1,28 @@
+package com.acmeair.hystrix;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@Profile("SpringCloud")
+public class SpringCloudSeckillCommand extends SeckillCommand {
+    private static final Logger logger = LoggerFactory.getLogger(SpringCloudSeckillCommand.class);
+
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
+    SpringCloudSeckillCommand() {
+        super(new RestTemplate());
+    }
+
+    protected String getSeckillServiceAddress() {
+        String address = loadBalancer.choose(seckillServiceName).getUri().toString();
+        logger.info("Just get the address {} from LoadBalancer.", address);
+        return address + "/rest";
+    }
+}
